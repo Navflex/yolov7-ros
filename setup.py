23a425c42e5112293fe993112f59675dbdf2a7ff
Alexander Paul Wansiedler <apwansiedler@gmail.com>
2025-09-23 12:53:00 +0000

    install fixes

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from distutils.core import setup
 
 from catkin_pkg.python_setup import generate_distutils_setup
 
-d = generate_distutils_setup(packages=["yolov7-ros"], package_dir={"": "src"})
+d = generate_distutils_setup(packages=["utils", "models", "visualizer"], package_dir={"": "src"})
 
 setup(**d)
