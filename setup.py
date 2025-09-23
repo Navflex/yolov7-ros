6bcd9db10011c0ffbc9577506cd10214db0d60b0
Alexander Paul Wansiedler <apwansiedler@gmail.com>
2025-09-23 12:53:00 +0000

    fx

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from distutils.core import setup
 
 from catkin_pkg.python_setup import generate_distutils_setup
 
-d = generate_distutils_setup(packages=["yolov7-ros"], package_dir={"": "src"})
+d = generate_distutils_setup(packages=["utils"], package_dir={"": "src"})
 
 setup(**d)
