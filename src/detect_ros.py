41b000b440fd3586a74427d81968e7b2889d5978
nicholas-nf <nicholas@navflex.com>
2026-06-08 13:26:16 -0600

    Revert "Merge branch 'feat/log_yolo_model_load' into feat/airbag_retraining"
    This reverts commit 6a32fb63d6f5916599a343d6dbfae32520bb50a9, reversing
    changes made to e58b7ceb45a8c2fd9703d4d48b4d21202235fec3.

--- src/detect_ros.py
+++ src/detect_ros.py
@@ -55,7 +55,6 @@ class YoloV7:
         self.device = device
         self.model = attempt_load(weights, map_location=device)
         self.model.eval()
-        rospy.loginfo(f"YoloV7 model loaded successfully from: {weights}")
 
     @torch.no_grad()
     def inference(self, img: torch.Tensor):
