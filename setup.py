c0493f0bba7da192fca00d5ef20ec92435d49167
rik-nf <rik@navflex.com>
2026-08-04 11:06:29 -0600

    Release 2026.1.1 (#16)
    * warning fix
    
    * install fixes
    
    * Add Codeowners and PR Template (#6)
    
    * fx
    
    * add codeowners, PR template
    
    ---------
    
    Co-authored-by: Alexander Paul Wansiedler <apwansiedler@gmail.com>
    
    * Feat: Update `CODEOWNERS` (#9)
    
    * Update CODEOWNERS
    
    * Feat: Support Cython and Remove Hardcoded Paths (#10)
    
    * build: modernize setup.py for yolov7-ros
    
    * setuptools/install fix
    
    * fix(yolov7-ros): update gitignore file to ignore pycache folder
    
    * CMakeLists.txt weights install fix
    
    * unhardcoded paths (#8)
    
    ---------
    
    Co-authored-by: Sergio Catarino <sergio@navflex.com>
    Co-authored-by: Navflex <info@navflex.com>
    
    * Feat: Add Jungheinrich POC Weights (#12)
    
    * add jh poc weights
    
    * Feat: Retrained Model on Dallas / Broomfield Airbag Data (#13)
    
    * feat: updated model after retraining on airbag data
    
    * Added log message for successfully loading weights
    
    * Revert "Merge branch 'feat/log_yolo_model_load' into feat/airbag_retraining"
    
    This reverts commit 6a32fb63d6f5916599a343d6dbfae32520bb50a9, reversing
    changes made to e58b7ceb45a8c2fd9703d4d48b4d21202235fec3.
    
    ---------
    
    Co-authored-by: Egor <egor@navflex.com>
    Co-authored-by: nicholas-nf <nicholas@navflex.com>
    
    * Feat: Two Class YOLO (#15)
    
    Co-authored-by: nicholas-nf <nicholas@navflex.com>
    
    ---------
    
    Co-authored-by: wansiedler <apwansiedler@gmail.com>
    Co-authored-by: austin-nf <austin@navflex.com>
    Co-authored-by: Sergio Catarino <sergio@navflex.com>
    Co-authored-by: Navflex <info@navflex.com>
    Co-authored-by: Egor <egor@navflex.com>
    Co-authored-by: nicholas-nf <nicholas@navflex.com>

--- setup.py
+++ setup.py
@@ -1,7 +1,45 @@
-from distutils.core import setup
-
+from setuptools import setup
+from setuptools.command.install import install as _install
 from catkin_pkg.python_setup import generate_distutils_setup
+import warnings
 
-d = generate_distutils_setup(packages=["utils", "models", "visualizer"], package_dir={"": "src"})
+# Suppress the "setup.py install is deprecated" warning
+warnings.filterwarnings("ignore", message="setup.py install is deprecated")
+
+# --------------------------------------------------------------------------------
+# Custom Install Command
+# --------------------------------------------------------------------------------
+class Install(_install):
+    """
+    Custom Install command to handle the --install-layout flag.
+    This ensures compatibility with the standard ROS/Ubuntu dist-packages layout.
+    """
+    user_options = _install.user_options + [
+        ('install-layout=', None, "installation layout")
+    ]
+    def initialize_options(self):
+        _install.initialize_options(self)
+        self.install_layout = None
+    
+    def finalize_options(self):
+        _install.finalize_options(self)
+    
+    def run(self):
+        if self.install_lib and 'site-packages' in self.install_lib:
+             self.install_lib = self.install_lib.replace('site-packages', 'dist-packages')
+             self.install_lib = self.install_lib.replace('python3.8', 'python3')
+
+        if self.install_purelib and 'site-packages' in self.install_purelib:
+             self.install_purelib = self.install_purelib.replace('site-packages', 'dist-packages')
+             self.install_purelib = self.install_purelib.replace('python3.8', 'python3')
+
+        _install.run(self)
+
+d = generate_distutils_setup(
+    packages=["utils", "models", "visualizer"], 
+    package_dir={"": "src"}
+)
+
+d['cmdclass'] = {'install': Install}
 
 setup(**d)
